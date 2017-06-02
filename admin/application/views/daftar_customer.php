d592e0bde04de751cbf5e8e74e09665674f17983
M Zhafran/E41151242 <zhafranku@gmail.com>
2017-06-02 07:47:52 +0700

    daftar customer

--- admin/application/views/daftar_customer.php
+++ admin/application/views/daftar_customer.php
@@ -10,7 +10,6 @@
             <th>Alamat</th>
             <th>Kode Ver</th>
             <th>Rating</th>
-            <th>Tindakan</th>
         </tr>
     </thead>
     <tbody>
@@ -25,25 +24,7 @@
             <td><?php echo $row->alamat;?></td>
             <td><?php if($row->kode_ver<>"")echo '<b class="text-primary">'.$row->kode_ver.'</b>';?></td>
             <td><?php echo $row->rating;?></td>
-            <td align="center"><a href="#" data-toggle="modal" data-target="#modal_edit" class="btn btn-xs btn-warning btnEdit"><span class="glyphicon glyphicon-pencil"></span></a>
-            <a href="#" data-toggle="modal" data-target="#modal_hapus" class="btn btn-xs btn-danger btnHapus"><span class="glyphicon glyphicon-trash"></span> </a></td>
         </tr>
     <?php } ?>
     </tbody>
 </table>
-<script>
-$(function(){
-	$('.btnKonfirmasi').click(function(){
-		$('#id_teknisi').val($(this).attr('id_teknisi'));
-		$.ajax({
-			type: "POST",
-			url: "<?php echo site_url('admin/ajaxFormKonfTeknisi/'); ?>",
-			//dataType: 'json',
-			data: {id_teknisi: $(this).attr('id_teknisi')},
-			success: function(data) {
-				$('#resultFormKonf').html(data);
-			}
-		});
-	});
-});
-</script>
